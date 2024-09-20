3a981832efaf956609fdf15d6c99364802192378
braucktoon <braucktoon@gmail.com>
2024-09-20 13:18:57 -0400

    Install script

--- install.sh
+++ install.sh
@@ -2,7 +2,37 @@
 
 # Variables
 TARGET_DIR="/var/lib/vz/snippets"   # Replace with your target directory path
-SOURCE_DIR="."
+SOURCE_DIR="./home-infra/cloud-init/apps"
+# GitHub repository URL
+REPO_URL="https://github.com/braucktoon/home-infra.git"
+# Directory where the repo will be cloned
+CLONE_DIR="."
+
+# Function to handle errors
+error_exit() {
+    echo "$1" 1>&2
+    exit 1
+}
+
+# Check if git is installed, if not, install it
+if ! command -v git &> /dev/null; then
+    echo "Git not found. Installing git..."
+    apt-get update || error_exit "Failed to update package list. Exiting..."
+    apt-get install -y git || error_exit "Failed to install git. Exiting..."
+else
+    echo "Git is already installed."
+fi
+
+# Clone the repository
+echo "Cloning repository from $REPO_URL..."
+git clone "$REPO_URL" "$CLONE_DIR" || error_exit "Failed to clone repository. Exiting..."
+
+# Check if the clone was successful
+if [ -d "$CLONE_DIR" ]; then
+    echo "Repository successfully cloned into $CLONE_DIR."
+else
+    error_exit "Repository not cloned. Exiting..."
+fi
 
 # Check if the directory exists
 if [ ! -d "$TARGET_DIR" ]; then
@@ -21,4 +51,39 @@ else
   echo "No .yaml files found in $SOURCE_DIR."
 fi
 
-echo "Script completed."
+# Call additional scripts with error checking
+SCRIPT1="./cloud-init/debian/debian-12-cloudinit.sh"
+SCRIPT2="./cloud-init/debian/debian-12-cloudinit+docker.sh"
+
+if [ -f "$SCRIPT1" ] && [ -f "$SCRIPT2" ] && [ -f "$SCRIPT3" ]; then
+  echo "Calling script1.sh..."
+  bash "$SCRIPT1"
+  if [ $? -ne 0 ]; then
+    echo "Error: script1.sh failed." >&2
+    exit 1
+  fi
+
+  echo "Calling script2.sh..."
+  bash "$SCRIPT2"
+  if [ $? -ne 0 ]; then
+    echo "Error: script2.sh failed." >&2
+    exit 1
+  fi
+
+else
+  echo "One or more scripts not found. Please check the paths."
+  exit 1
+fi
+
+# Run the haos-vm.sh script and check if it completes successfully
+echo "Running haos-vm.sh from GitHub..."
+bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/vm/haos-vm.sh)"
+if [ $? -ne 0 ]; then
+  echo "Error: haos-vm.sh failed to run." >&2
+  exit 1
+else
+  echo "haos-vm.sh completed successfully."
+fi
+
+echo "All tasks completed successfully."
+
